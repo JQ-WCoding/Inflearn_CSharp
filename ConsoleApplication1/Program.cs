e53e76d6e895ede10f064cb0a63f6b4a531fc517
Jun_Q <junlee36520620@gmail.com>
2022-02-13 22:07:58 +0900

    Initial commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // boolean 1byte
+            bool b = true;
+            b = false;
+            Console.WriteLine("{0}", b);
+
+            // float 4bytes
+            float f = 3.14f;
+
+            // double 8bytes
+            double d = 3.14;
+
+            // char 2bytes
+            char c = 'A';
+
+            // string 
+            string s = "Hello";
+
+            Console.WriteLine(s);
+        }
+    }
+}
